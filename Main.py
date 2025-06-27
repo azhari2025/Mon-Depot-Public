c2af97639085acbc8492fce721126adc64002224
azhari2025 <6354972@bdeb.qc.ca>
2025-06-27 11:33:51 -0400

    Update Main.py
    commit pour les tests unitaire

--- Main.py
+++ Main.py
@@ -1,6 +1,19 @@
-print("hello word")
-a=4
-b=6
-c=a+b
-print("resultat",c)
-################
+import unittest
+
+# Fonction à tester
+def add(a, b):
+    return a + b
+
+# Classe de test
+class TestAddition(unittest.TestCase):
+
+    def test_addition(self):
+        # Appel de la fonction à tester
+        result = add(4, 5)
+
+        # Assertion pour vérifier si le résultat est conforme à ce qui est attendu
+        self.assertEqual(result, 9, "Le résultat attendu est 9")
+
+# Pour exécuter les tests
+if __name__ == '__main__':
+    unittest.main()
