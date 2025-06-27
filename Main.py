05f4c5e087fdf2450c63a199b910d77f00cc6000
azhari2025 <6354972@bdeb.qc.ca>
2025-06-27 11:51:25 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,6 @@ class TestAddition(unittest.TestCase):
 
         # Assertion pour vérifier si le résultat est conforme à ce qui est attendu
         self.assertEqual(result, 9, "Le résultat attendu est 9")
-
-# Pour exécuter les tests
-if __name__ == '__main__':
-    unittest.main()
+        
+test_addition = unittest.TestLoader().loadTestsFromTestCase(TestAddition)
+unittest.TextTestRunner().run(test_addition)
