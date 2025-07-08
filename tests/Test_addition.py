7bd8176485aa4e9367459e2e96393b28a83e52a7
azhari2025 <6354972@bdeb.qc.ca>
2025-07-08 08:28:21 -0400

    modification structure

---
+++ tests/Test_addition.py
@@ -0,0 +1,18 @@
+import unittest
+
+# Fonction à tester
+def add(a, b):
+    return a + b
+
+# Classe de test
+class TestAddition(unittest.TestCase):
+
+    def test_addition(self):
+        # Appel de la fonction à tester
+        result = add(4, 5)
+
+        # Assertion pour vérifier si le résultat est conforme à ce qui est attendu
+        self.assertEqual(result, 9, "Le résultat attendu est 9")
+# execution des tests        
+test_addition = unittest.TestLoader().loadTestsFromTestCase(TestAddition)
+unittest.TextTestRunner().run(test_addition)
